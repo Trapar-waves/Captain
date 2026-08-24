afcb9d4449b1822f22e17cb8d7f4a805cbe74256
rikka <admin@rikka.cc>
2026-08-24 19:33:26 +0800

    chore: replace @antfu/eslint-config with @renton/eslint-config

--- src/package.ts
+++ src/package.ts
@@ -1,21 +1,9 @@
-import cliTemplate from "@trapar-waves/cli-template/package.json";
-import llmTemplate from "@trapar-waves/llm-template/package.json";
-import reactAntdPro from "@trapar-waves/react-antd-pro/package.json";
-import reactMantineTailwind from "@trapar-waves/react-mantine-tailwind/package.json";
-import reactTailwind from "@trapar-waves/react-tailwind/package.json";
-import reactTanstack from "@trapar-waves/react-tanstack/package.json";
-import reactThreeMaplibre from "@trapar-waves/react-three-maplibre/package.json";
-import reactVisglMaplibre from "@trapar-waves/react-visgl-maplibre/package.json";
-import vueTailwind from "@trapar-waves/vue-tailwind/package.json";
-
-export {
-  cliTemplate,
-  llmTemplate,
-  reactAntdPro,
-  reactMantineTailwind,
-  reactTailwind,
-  reactTanstack,
-  reactThreeMaplibre,
-  reactVisglMaplibre,
-  vueTailwind,
-};
+export { default as cliTemplate } from "@trapar-waves/cli-template/package.json";
+export { default as llmTemplate } from "@trapar-waves/llm-template/package.json";
+export { default as reactAntdPro } from "@trapar-waves/react-antd-pro/package.json";
+export { default as reactMantineTailwind } from "@trapar-waves/react-mantine-tailwind/package.json";
+export { default as reactTailwind } from "@trapar-waves/react-tailwind/package.json";
+export { default as reactTanstack } from "@trapar-waves/react-tanstack/package.json";
+export { default as reactThreeMaplibre } from "@trapar-waves/react-three-maplibre/package.json";
+export { default as reactVisglMaplibre } from "@trapar-waves/react-visgl-maplibre/package.json";
+export { default as vueTailwind } from "@trapar-waves/vue-tailwind/package.json";
